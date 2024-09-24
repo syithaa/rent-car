08078f956a1f1a2c004543e27d3cd10b7ec3865d
Syahrotul <syahrotulsyitha03@gmail.com>
2024-09-24 11:03:52 +0700

    membuat migration dan models

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaksi extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'transaksis';
+    protected $primaryKey = 'id';
+    protected $fillable = ['user_id', 'mobil_id', 'nama', 'ponsel', 'alamat', 'lama', 'tgl_pesan', 'tgl_kembali', 'total', 'status'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mobil(): BelongsTo
+    {
+        return $this->belongsTo(Mobil::class);
+    }
+}
---
+++ database/migrations/2024_09_24_022017_create_transaksis_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use App\Models\Mobil;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Mobil::class);
+            $table->string('nama')->nullable();
+            $table->string('ponsel')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('lama')->nullable();
+            $table->date('tgl_pesan')->nullable();
+            $table->date('tgl_kembali')->nullable();
+            $table->string('total')->nullable();
+            $table->enum('status', ['WAIT', 'PROSES', 'SELESAI'])->nullable();
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
